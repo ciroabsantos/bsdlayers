b738c7ad5932ed52a425ce2dce2c06b99af4b44e
ciroabsantos <ciroabsantos@gmail.com>
2019-10-23 20:00:50 -0300

    creates interface for business commands #1

---
+++ src/BsdLayers.Business/Commands/IBusinessCommand.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+
+namespace BsdLayers.Business.Commands
+{
+    /// <summary>
+    /// Interface for Business Commands
+    /// </summary>
+    public interface IBusinessCommand
+    {
+        Task<BusinessCommandResult> ExecuteAsync();
+
+        Task<BusinessCommandResult> RollBackAsync();
+    }
+
+    /// <summary>
+    /// Interface for commands that produces a content in the result
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IBusinessCommand<T> : IBusinessCommand
+    {
+        Task<BusinessCommandResult<T>> ExecuteForContentAsync();
+    }
+}
